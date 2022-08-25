dfc7d56848e952eecad46d7245b77fb0b54c536e
ArtemPyanov <temih180696@yandex.ru>
2022-08-25 23:59:08 +0300

    complete the homework1

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+// Задача 2. Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Input first number ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 > n2)
+    Console.WriteLine($"{n1} is max {n2} is min"); 
+else
+{
+    Console.WriteLine($"{n1} is min {n2} is max");
+}
+*/
+
+/*
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Input first number ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input third number ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+int max = n1;
+if ( n1 > max ) max = n1;
+if ( n2 > max ) max = n2;
+if ( n3 > max ) max = n3;
+Console.WriteLine($"{max} is max ");
+*/
+/*
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Input number ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+if (n1 % 2 == 0 ) 
+Console.WriteLine($"Even is {n1} ");
+else
+{
+    Console.WriteLine($"{n1} is not Even ");
+}
+*/
